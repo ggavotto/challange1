97665dc060a984e30471e215033a0892ff6c4ce1
CRACIUN,ROMULUS COSMIN <romulus.craciun@servexternos.gruposantander.com>
2021-09-30 12:50:33 +0200

    Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,7 @@ import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.annotations.QuarkusMain;
 
 @QuarkusMain
-public class LibraryApplication {
+public class Main {
 
 	public static void main(String[] args) {
 	     System.out.println("Running main method");
